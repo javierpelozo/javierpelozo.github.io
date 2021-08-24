bcd29c1f7846f3e8a89388ee6eb02a58e4218714
javierpelozo <javpelozo@gmail.com>
2021-08-24 18:07:08 -0400

    statically

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/javierpelozo/javierpelozo.github.io/gh-pages/' : '',
 }
